b9107d9334085e51d7d278eaf3fa2609990bc20a
Ubuntu <ubuntu@ip-172-31-21-18.us-east-2.compute.internal>
2021-09-20 07:43:38 +0000

    added file path

--- xkcd.py
+++ xkcd.py
@@ -1,13 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
-number = 1
+import urllib.request
+from datetime import date, datetime, timedelta
+
+
+base_date = '2021-09-19'
+date_t = date.today()
+number = date_t - base_date
 url = "https://xkcd.com/"+str(number)
 urlreq = requests.get(url)
 html = BeautifulSoup(urlreq.content, "html.parser")
-print(html)
 
 html_title = html.find("div", id="ctitle")
+
+urls = [a["href"] for a in html.find_all('a', href=True) if a["href"][a["href"].rfind(".")+1:] in ["jpeg", "png", "jpg"]]
+url_value= urls[0]
 title = html_title.get_text()
 print(title)
+print(url_value)
 
+filename = '/home/ubuntu/files/xkcdfolder/'+'('+str(number)+')'+'-'+str(title)+".jpg"
 
+urllib.request.urlretrieve(url_value, filename)
